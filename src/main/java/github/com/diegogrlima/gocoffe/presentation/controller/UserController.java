7dafae17494ac00a55e381cf8fa1eee5fc1a166e
Diego Lima <diegolimareis021@gmail.com>
2026-06-23 07:21:01 -0300

    fix: adiciona @PreAuthorize ADMIN no POST /users

--- src/main/java/github/com/diegogrlima/gocoffe/presentation/controller/UserController.java
+++ src/main/java/github/com/diegogrlima/gocoffe/presentation/controller/UserController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,6 +21,7 @@ public class UserController {
     private final CreateUserUseCase createUserUseCase;
 
     @PostMapping
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<CreateUserOutput> create(@Valid @RequestBody CreateUserInput input) {
         CreateUserOutput output = createUserUseCase.execute(input);
         return ResponseEntity.status(HttpStatus.CREATED).body(output);
