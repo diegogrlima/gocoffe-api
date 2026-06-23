4c183c6aba1401895ab9838fd10a2ea517dacf39
Diego Lima <diegolimareis021@gmail.com>
2026-06-23 07:38:34 -0300

    refactor: altera ProductImage de ManyToOne para OneToOne com Product

--- src/main/java/github/com/diegogrlima/gocoffe/infrastructure/persistence/product/ProductImageJpaEntity.java
+++ src/main/java/github/com/diegogrlima/gocoffe/infrastructure/persistence/product/ProductImageJpaEntity.java
@@ -7,7 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,8 +34,8 @@ public class ProductImageJpaEntity {
     @Column(name = "image_url", nullable = false)
     private String imageURL;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "product_id", nullable = false)
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "product_id", nullable = false, unique = true)
     private ProductJpaEntity product;
 
     @CreationTimestamp
